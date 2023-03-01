7307cf04e27893a2e2310310a4277bb53bde6a7a
alexismagnin <57742544+alexismagnin@users.noreply.github.com>
2023-03-01 01:15:57 -0300

    primer commit

---
+++ formulario.js
@@ -0,0 +1,6 @@
+document.getElementById("formulario").addEventListener("submit", function () {
+    document.getElementById("formulario").style.display = 'none';
+    const nombre = document.getElementById("input-nombre").value;
+    const apellido = document.getElementById("input-apellido").value;
+    document.getElementById("div-formulario").innerHTML = "<strong>" + nombre + " " + apellido + ", gracias por contactarte!</strong>";
+})
---
+++ redireccionar.js
@@ -0,0 +1,11 @@
+document.getElementById("boton-datos").addEventListener("click", function () {
+    location.href="./datos-personales.html";
+});
+
+document.getElementById("boton-fortalezas").addEventListener("click", function () {
+    location.href="./fortalezas.html";
+});
+
+document.getElementById("boton-contacto").addEventListener("click", function () {
+    location.href="./form-contacto.html";
+});
