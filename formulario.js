976445337e98ebb1931b099634fa997a42f7812c
amagnin <alexis.magnin@gmail.com>
2023-03-01 01:44:57 -0300

    corrección labels

--- formulario.js
+++ formulario.js
@@ -2,5 +2,6 @@ document.getElementById("formulario").addEventListener("submit", function () {
     document.getElementById("formulario").style.display = 'none';
     const nombre = document.getElementById("input-nombre").value;
     const apellido = document.getElementById("input-apellido").value;
+    document.getElementById("div-formulario").style.textAlign = "center";
     document.getElementById("div-formulario").innerHTML = "<strong>" + nombre + " " + apellido + ", gracias por contactarte!</strong>";
-})
+});
